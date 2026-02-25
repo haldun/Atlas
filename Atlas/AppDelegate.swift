8af1412617bed3425e773b56aad6969d708befa4
Haldun Bayhantopcu <hbayhantopcu@gmail.com>
2026-02-25 01:03:52 +0100

    Initial Commit

---
+++ Atlas/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Atlas
+//
+//  Created by Haldun Bayhantopcu on 25.02.26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ AtlasTests/AtlasTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasTests.swift
+//  AtlasTests
+//
+//  Created by Haldun Bayhantopcu on 25.02.26.
+//
+
+import Testing
+@testable import Atlas
+
+struct AtlasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
